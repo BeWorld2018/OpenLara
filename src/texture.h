863e8a484ba019f1b2d772c9353077b6332885a7
BSzili <col.lawrence@gmail.com>
2025-05-16 16:17:58 +0200

    make setFilterQuality in Texture _GAPI_GL only

--- src/texture.h
+++ src/texture.h
@@ -50,6 +50,20 @@ struct Texture : GAPI::Texture {
                 for (int i = 0; i < tilesCount; i++)
                     this->tiles[i] = new Texture(tiles[i].width, tiles[i].height, 1, FMT_RGBA, /*OPT_NEAREST*/0, tiles[i].data);
             }
+
+            void setFilterQuality(int value) {
+                for (int i = 0; i < COUNT(tiles); i++) {
+                    if (tiles[i]) {
+                        tiles[i]->setFilterQuality(value);
+                    }
+                }
+
+                if (this->opt & OPT_PROXY) {
+                    return;
+                }
+
+                GAPI::Texture::setFilterQuality(value);
+            }
         #endif
 
         void bindTile(uint16 tile, uint16 clut) {
@@ -60,20 +74,6 @@ struct Texture : GAPI::Texture {
         #else
             tiles[tile]->bind(0);
         #endif
-        }
-
-        void setFilterQuality(int value) {
-            for (int i = 0; i < COUNT(tiles); i++) {
-                if (tiles[i]) {
-                    tiles[i]->setFilterQuality(value);
-                }
-            }
-
-            if (this->opt & OPT_PROXY) {
-                return;
-            }
-
-            GAPI::Texture::setFilterQuality(value);
         }
     #endif
 
